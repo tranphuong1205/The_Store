5d10c12414053be731783d887ba4330491c88e5e
tranphuong1205 <phuongxxx971022@gmail.com>
2023-09-25 00:17:40 +0700

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
