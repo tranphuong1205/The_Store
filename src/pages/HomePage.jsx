169b867c74620806ae907bd1033cead73d08be44
tranphuong1205 <phuongxxx971022@gmail.com>
2024-03-03 11:53:42 +0700

    add prettier

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,28 @@
 const HomePage = () => {
-  return (
-    <div id="continer" class="content">
-      <div id="intro" class="intro">
-        <div class="sale">
-          <p class="sale1">
-            Sale
-            <br />
-          </p>
-          <p class="sale2">
-            Up To 50% Off
-            <br />{" "}
-          </p>
+    return (
+        <div id="continer" class="content">
+            <div id="intro" class="intro">
+                <div class="sale">
+                    <p class="sale1">
+                        Sale
+                        <br />
+                    </p>
+                    <p class="sale2">
+                        Up To 50% Off
+                        <br />{' '}
+                    </p>
+                </div>
+            </div>
+            <div class="reference">
+                <div class="woman">
+                    <img src="/Fashion_Week_02.jpg" />
+                </div>
+                <div class="man">
+                    <img src="/clothes.png" />
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="reference">
-        <div class="woman">
-          <img src="/Fashion_Week_02.jpg" />
-          <div class="text">Coats</div>
-        </div>
-        <div class="man">
-          <img src="/dress2.png" />
-          <div class="text">Dress</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HomePage;
